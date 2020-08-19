34928a11ce1a1de2f3a61a3770b74b4573a02741
19013078 <19013078@DESKTOP-TRGI9NF>
2020-08-19 23:59:16 +0800

    Test whether able to view, add and archive transaction -Zun Hui

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,47 +3,12 @@ import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		ArrayList<Product> productList = new ArrayList<Product>();
-
-		productList.add(new Product(00, "Pork", "Food", "China Pork", "LiuJiaZhuRou"));
-		productList.add(new Product(01, "Toothbrush", "Household", "Janpan toothbrush", "YuKu Brush"));
-
-		int option = 0;
-
-		while (option != 5) {
-
-			C206_CaseStudy.menu();
-			option = Helper.readInt("Enter an option > ");
-
-			if (option == 1) {
-				// View all product
-				C206_CaseStudy.viewAllProduct(productList);
-			} else if (option == 2) {
-
-				// Add product
-
-				C206_CaseStudy.addProduct(productList);
-
-			} else if (option == 3) {
-
-				// delete product
-				C206_CaseStudy.viewAllProduct(productList);
-
-			} else if (option == 5) {
-				System.out.println("Bye!");
-			} else {
-				System.out.println("Invalid option");
-			}
-
-		}
-=======
 		// TODO Auto-generated method stub
 		ArrayList<Transaction> TransactionList = new ArrayList<Transaction>();
 		ArrayList<Transaction> ArchiveTransactionList = new ArrayList<Transaction>();
 int option = -1;
 		
-		while (option != 6) {
+		while (option != 5) {
 
 			System.out.println("1. Transaction");
 			System.out.println("2. -");
@@ -96,7 +61,16 @@ public static void addTransaction(ArrayList<Transaction> TransactionList) {
 		Transaction tra=new Transaction(transcationID, customername, productname, staffname);
 		TransactionList.add(tra);
 		System.out.println("Transaction added");
+		
+		
 	}
+public static void addTransaction(ArrayList<Transaction> transactionList, Transaction t) {
+
+    transactionList.add(t);
+    System.out.println("Transaction added");
+  }
+  
+
 	//=================view all=================================
 public static String retrieveAllTransaction(ArrayList<Transaction> TransactionList) {
 	String output="";
@@ -130,91 +104,4 @@ public static void archiveOldTransaction(ArrayList<Transaction> TransactionList,
 		
 	}
 
-
->>>>>>> branch 'master' of https://github.com/19012453-MinShuang/C206_CaseStudy.git
-
-	}
-
-<<<<<<< HEAD
-	public static void menu() {
-		C206_CaseStudy.setHeader("RESOURCE CENTRE APP");
-		System.out.println("1. Display Inventory");
-		System.out.println("2. Add item");
-		System.out.println("3. Delete item");
-		System.out.println("5. Quit");
-		Helper.line(80, "-");
-
-	}
-
-	public static void setHeader(String header) {
-		Helper.line(80, "-");
-		System.out.println(header);
-		Helper.line(80, "-");
-
-	}
-
-	// Write the code for the viewAllProduct method.
-	// Note: This method is a public static method that takes in an ArrayList of
-	// Product objects.
-	public static void viewAllProduct(ArrayList<Product> productList) {
-		Helper.line(45, "=");
-		System.out.println("Product List");
-		Helper.line(45, "=");
-
-		if (productList.isEmpty()) {
-			System.out.println("There is no product available.");
-		} else {
-			String out = String.format("%-10s  %-5s %-15s %-20s %-5s\n ", "Product ID ", "Product Description",
-					"Product Category ", "Product Name", "Vendor Name");
-			for (int i = 0; i < productList.size(); i++) {
-				Product ppp = productList.get(i);//
-				out += String.format("%-10d %-5s %-20s  %-15s %-5s\n", ppp.getProductID(), ppp.getProductDescription(),
-						ppp.getProductCategory(), ppp.getProductName(), ppp.getVendorName());
-			}
-			System.out.println(out);
-		}
-	}
-
-	// add
-	public static void addProduct(ArrayList<Product> productList) {
-		int productID = Helper.readInt("Enter product ID: ");
-		String productDescription = Helper.readString("Enter  product description: ");
-		String productCategory = Helper.readString("Enter product category : ");
-		String productName = Helper.readString("Enter product name: ");
-		String vendorName = Helper.readString("Enter vendor name  : ");
-
-		productList.add(new Product(productID, productDescription, productCategory, productName, vendorName));
-		System.out.println("Product added");
-
-	}
-
-	public static void removefromlist(ArrayList<Product> productList) {
-		Helper.line(45, "=");
-		System.out.println("Remove");
-		Helper.line(45, "=");
-
-		boolean isFound = false;
-		int id = Helper.readInt("Enter peoduct ID to delete item: ");
-		String confirm = Helper.readString("Are you sure you want to delete product from list?(Y/N): ");
-
-		for (int i = 0; i < productList.size(); i++) {
-			if (productList.get(i).getProductID() == id) {
-				if (confirm.equalsIgnoreCase("y")) {
-					isFound=true;
-					
-				}
-			} else {
-				System.out.println("Invalid Product ID");
-
-			}
-			if(isFound==true) {
-				productList.remove(i);
-			}
-
-		}
-	}
-	//
 }
-=======
-
->>>>>>> branch 'master' of https://github.com/19012453-MinShuang/C206_CaseStudy.git
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,13 +1,38 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	private Transaction t1;
+	private Transaction t2;
+	
+	
+	private ArrayList<Transaction> TransactionList;
+	private ArrayList<Transaction> ArchiveList;
 
+	
+	
+	public C206_CaseStudyTest() {
+		super();
+	}
+	
+	
+	
+	
+	
 	@Before
 	public void setUp() throws Exception {
+		//Prepare the test data
+		
+		//Transaction ID
+		t1 = new Transaction(9000,"John","container","Mary");
+		t2 =new Transaction(9000,"John","container","Mary");
+		
+
 	}
 
 	@After
@@ -16,8 +41,28 @@ public class C206_CaseStudyTest {
 
 	@Test
 	public void c206_test() {
+		  
+			    // Item list is not null, so that can add a new item
+			    assertNotNull("Test if there is valid Transaction arraylist to add to", TransactionList);
+			    
+			    //Given an empty list, after adding 1 item, the size of the list is 1
+			    C206_CaseStudy.addTransaction(TransactionList, t1);    
+			    assertEquals("Test if that Transaction arraylist size is 1?", 1, TransactionList.size());
+			    
+			    //The item just added is as same as the first item of the list
+			    assertSame("Test that Transaction is added same as 1st item of the list?", t1, TransactionList.get(0));
+			    
+			    //Add another item. test The size of the list is 2?
+			    C206_CaseStudy.addTransaction(TransactionList, t2);
+			    assertEquals("Test that Transaction arraylist size is 2?", 2, TransactionList.size());
+			  
+			  
+
+
+		
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
+		
 	}
 
 }
